3c333388aedb3b903ab155aab8281b88aa4a7dd0
Jiří <j.laudat@email.cz>
2023-03-14 09:34:24 +0100

    refactore styles

--- src/components/styles/Global.ts
+++ src/components/styles/Global.ts
@@ -14,6 +14,7 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: arial,sans-serif;
 
     &:before {
         box-sizing: border-box;
@@ -26,9 +27,6 @@ export const GlobalStyles = createGlobalStyle`
 
 
 body {
-
-        font-family: arial,sans-serif;
         background: #fbfbf9;
-        margin: 0;
     }
 `;
